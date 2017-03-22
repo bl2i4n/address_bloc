50b803d83c34ebe55184d6c307e58dc97a957f36
bl2i4n <bl2i4n@yahoo.com>
2017-03-22 16:25:43 -0700

    end of checkpoint 6, menu and submenu work for viewing entries and adding

--- controllers/menu_controller.rb
+++ controllers/menu_controller.rb
@@ -23,22 +23,21 @@ class MenuController
         system "clear"
         view_all_entries
         main_menu
-      when 1
+      when 2
         system "clear"
-        view_all_entries
+        create_entry
         main_menu
-      when 1
+      when 3
         system "clear"
-        view_all_entries
+        search_entries
         main_menu
-      when 1
+      when 4
         system "clear"
-        view_all_entries
+        read_csv
         main_menu
-      when 1
-        system "clear"
-        view_all_entries
-        main_menu
+      when 5
+        puts "Good-bye!"
+        exit(0)
       else
         system "clear"
         puts "Sorry, that is not a valid input"
@@ -47,13 +46,64 @@ class MenuController
     end
 
     def view_all_entries
+      address_book.entries.each do |entry|
+        system "clear"
+        puts entry.to_s
+
+        entry_submenu(entry)
+      end
+
+      system "clear"
+      puts "End of entries"
     end
 
     def create_entry
+      system "clear"
+      puts "New AddressBloc Entry"
+
+      print "Name: "
+      name = gets.chomp
+      print "Phone number: "
+      phone = gets.chomp
+      print "Email: "
+      email = gets.chomp
+
+      address_book.add_entry(name, phone, email)
+
+      system "clear"
+      puts "New entry created"
     end
 
     def search_entries
     end
 
     def read_csv
+    end
+
+    def entry_submenu(entry)
+      puts "n - next entry"
+      puts "d - delete entry"
+      puts "e - edit this entry"
+      puts "m - return to main menu"
+
+      selection = gets.chomp
+
+      case selection
+        when "n"
+
+        when "d"
+
+        when "e"
+
+        when "m"
+          system "clear"
+          main_menu
+        else
+          system "clear"
+          puts "#{selection} is not a valid input"
+          entry_submenu(entry)
+        end
+      end
+
+
   end
