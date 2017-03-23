9ea7470b5b6bcb5cb0b2673be4ade4303a89ee64
bl2i4n <bl2i4n@yahoo.com>
2017-03-22 20:43:26 -0700

    worked with Ryan on assignment for checkpoint-4

--- greeting.rb
+++ greeting.rb
@@ -1,9 +1,8 @@
 def greeting
-  first, second, third, fourth = ARGV
-    puts "#{first} #{second}"
-    puts "#{first} #{third}"
-    puts "#{first} #{fourth}"
-
+  firstOne = ARGV.shift
+  ARGV.each do |nThOne|
+    puts "#{firstOne} #{nThOne}"
+  end
 end
 
 greeting
